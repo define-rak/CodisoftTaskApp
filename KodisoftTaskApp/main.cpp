a442f44a6b9b866bd2a85512c3c06621124527cf
Oleksandr Rak <define.rak@gmail.com>
2014-11-26 15:26:44 +0200

    Realization of process restarting

--- KodisoftTaskApp/main.cpp
+++ KodisoftTaskApp/main.cpp
@@ -1,11 +1,42 @@
-#include <Windows.h>
-
+#include <windows.h>
 #include <iostream>
+#include <tchar.h>
+
 using namespace std;
 
-int main()
+void _tmain(int argc, TCHAR *argv[])
 {
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
 
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
 
-	return 0;
+	LPTSTR path = "C://python27//python.exe";
+
+	// Start the child process. 
+	do {
+		if (!CreateProcess(NULL,   // No module name (use command line)
+			path,        // Command line
+			NULL,           // Process handle not inheritable
+			NULL,           // Thread handle not inheritable
+			FALSE,          // Set handle inheritance to FALSE
+			CREATE_NEW_CONSOLE,              // No creation flags
+			NULL,           // Use parent's environment block
+			NULL,           // Use parent's starting directory 
+			&si,            // Pointer to STARTUPINFO structure
+			&pi)           // Pointer to PROCESS_INFORMATION structure
+			)
+		{
+			printf("CreateProcess failed (%d).\n", GetLastError());
+			return;
+		}
+	} while (WaitForSingleObject(pi.hProcess, INFINITE) == WAIT_OBJECT_0); // Wait until child process exits.
+
+	
+
+	// Close process and thread handles. 
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
 }
